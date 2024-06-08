777fe11671b0aaadeb94e759a373f9ba741fabfd
TanishBansal189 <150682418+TanishBansal189@users.noreply.github.com>
2024-06-08 16:55:05 +0530

    Add files via upload

---
+++ express/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+// Use the port from the environment variables or default to 3000
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
